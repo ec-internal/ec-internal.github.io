652efd2dd0687d6045506015bec156ae581e6665
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-01-24 17:13:56 +0100

    update wrong email

--- iframes/contactForm/783d80c9805f3895db24a9d2ae04f255/script.js
+++ iframes/contactForm/783d80c9805f3895db24a9d2ae04f255/script.js
@@ -17,7 +17,7 @@
      if (errors.length === 0) {
          Email.send({
              SecureToken: "91d94f52-3aa5-48d7-9f50-e15be2cdc264",
-             To: "office@everyonecodes.academy",
+             To: "office@everyonecodes.io",
              From: "hi@xschoenberger.com",
              Subject: `Contact Request from ${name}`,
              Body: `<h1 style="margin: 0; padding:10px 10px 0px 10px;">Contact Request</h1><small style="font-style:italic;font-weight:bold;color:grey;padding: 0px 10px 10px 10px;">from ${name}</small><p style="padding: 10px;">${message}</p><small style="padding: 10px;">entered email: ${email}</small>`,
