54db898b51c6574317c1e7cd9d9f4cf8f59d16a0
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-01-27 15:26:56 +0100

    more improvements

--- iframes/moreInfo/d41d8cd98f00b204e9800998ecf8427e/script.js
+++ iframes/moreInfo/d41d8cd98f00b204e9800998ecf8427e/script.js
@@ -1,23 +1,32 @@
- document.getElementById("submit").addEventListener("click", () => {
+ document.getElementById('close').addEventListener("click", () => {
+     document.getElementById('email-prompt').style.display = 'none';
+     document.getElementById('open').style.display = 'block';
+ })
+
+ document.getElementById('open').addEventListener("click", () => {
+     document.getElementById('email-prompt').style.display = 'flex';
+     document.getElementById('open').style.display = 'none';
+ })
+
+
+ document.getElementById("send").addEventListener("click", () => {
      let errors = [];
 
      // get all values from form
-     let name = document.getElementById("name").value;
      let email = document.getElementById("email").value;
-     let message = document.getElementById("message").value;
+     let accepted = document.getElementById("accept").checked;
 
      let success = false;
 
      //clear all errors from inputs
-     let fields = document.getElementsByClassName("input-field");
+     let fields = document.getElementsByClassName("input");
      for (let el of fields) {
          el.classList.remove("error");
      }
 
      //check for empty fields
-     if (name === "") errors.push("name");
      if (email === "") errors.push("email");
-     if (message === "") errors.push("message");
+     if (!accepted) errors.push("accept");
 
      if (errors.length === 0) {
 
@@ -26,8 +35,8 @@
              SecureToken: "91d94f52-3aa5-48d7-9f50-e15be2cdc264",
              To: "office@everyonecodes.io",
              From: "hi@xschoenberger.com",
-             Subject: `Contact Request from ${name}`,
-             Body: `<h1 style="margin: 0; padding:10px 10px 0px 10px;">Contact Request</h1><small style="font-style:italic;font-weight:bold;color:grey;padding: 0px 10px 10px 10px;">from ${name}</small><p style="padding: 10px;">${message}</p><small style="padding: 10px;">entered email: ${email}</small>`,
+             Subject: `Contact Request from `,
+             Body: `<h1 style="margin: 0; padding:10px 10px 0px 10px;">Contact Request</h1><small style="font-style:italic;font-weight:bold;color:grey;padding: 0px 10px 10px 10px;">from ${name}</small><p style="padding: 10px;">${accepted}</p><small style="padding: 10px;">entered email: ${email}</small>`,
          }).then((message) => {
              if (message === "OK") { //on successfull send
                  success = true;
@@ -36,6 +45,7 @@
              }
          });
      } else {
+         console.log(errors)
          for (let er of errors) { //add error class to every wrong field
              document.getElementById(er).classList.add("error");
          }
