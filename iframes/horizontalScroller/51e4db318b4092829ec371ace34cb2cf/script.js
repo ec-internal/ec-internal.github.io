7a62651077448abe9c02d485426811c9e9de0d45
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-02-24 17:03:42 +0100

    basic image loading

--- iframes/horizontalScroller/51e4db318b4092829ec371ace34cb2cf/script.js
+++ iframes/horizontalScroller/51e4db318b4092829ec371ace34cb2cf/script.js
@@ -1,17 +1,51 @@
+let images = [];
+
 window.onload = function() {
-    let id = document
-        .getElementById("eventbrite-button")
-        .getAttribute("eventId");
+    let decoded = hex2a('6768705f66643445765257424f4e6c537250336c6b6d4970324f7955593038474969344b466e6d4f');
+    let token = 'Bearer ' + decoded; //delete token and put into .env;
 
-    var exampleCallback = function() {
-        console.log("Order complete!");
-    };
+    axios({
+            method: 'get',
+            url: 'https://api.github.com/repos/ec-internal/ec-internal.github.io/contents/iframes/horizontalScroller/51e4db318b4092829ec371ace34cb2cf/images/',
+            responseType: 'stream',
+            headers: {
+                Authorization: token
+            }
+        })
+        .then(function(response) {
+            let data = response.data;
+            for (let d of data) {
+                axios({
+                        method: 'get',
+                        url: d.url,
+                        responseType: 'stream',
+                        headers: {
+                            Authorization: token
+                        }
+                    })
+                    .then(function(res) {
+                        let imageData = res.data;
+                        images.push(imageData.content)
+                        if (images.length == data.length) {
+                            renderImages();
+                        }
+                    });
 
-    window.EBWidgets.createWidget({
-        widgetType: "checkout",
-        eventId: id,
-        modal: true,
-        modalTriggerElementId: "eventbrite-button",
-        onOrderComplete: exampleCallback,
-    });
+            }
+        });
 };
+
+function renderImages() {
+    for (let img of images) {
+        $("#hor-scroll").append('<img src="data:image/jpg;base64,' + img + '" />');
+    }
+}
+
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2) {
+        var v = parseInt(hex.substr(i, 2), 16);
+        if (v) str += String.fromCharCode(v);
+    }
+    return str;
+}
