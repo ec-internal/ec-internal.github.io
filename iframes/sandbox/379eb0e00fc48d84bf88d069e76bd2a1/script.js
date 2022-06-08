3967a6700a9499044fa0051b815677537b13a92d
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-06-08 09:46:52 +0200

    dot

--- iframes/sandbox/379eb0e00fc48d84bf88d069e76bd2a1/script.js
+++ iframes/sandbox/379eb0e00fc48d84bf88d069e76bd2a1/script.js
@@ -1,3 +1,11 @@
 document.getElementById('dot').addEventListener('click', () => {
-    console.log("clicked")
+    let elId = Math.random() > 0.5 ? 1 : 0;
+    setOpacity(1, elId)
+    setTimeout(() => { setOpacity(0, elId) }, 1000)
 })
+
+function setOpacity(opacity, id) {
+    let image = document.getElementsByClassName('image')[id];
+    image.style.opacity = opacity;
+
+}
